e57fa457f334f5aed2e76301050509d6135f621b
sudharshanmadasu850-maker <sudharshanmadasu850@gmail.com>
2026-06-23 12:41:07 +0530

    Add files via upload

---
+++ add1.py
@@ -0,0 +1,10 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print(10+20)
+30
+>>> print(3*4)
+12
+>>> print("sudharshan")
+sudharshan
+>>> print(4-3)
+1
