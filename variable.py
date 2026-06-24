9cf9fe1fb1e1256b55d9d840576fff6c65da8d68
sudharshanmadasu850-maker <sudharshanmadasu850@gmail.com>
2026-06-24 18:58:42 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,83 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(3+3)
+6
+a=10
+print(a)
+10
+b=2
+c=4
+print(b+c)
+6
+x=20
+print(x)
+20
+y=30
+print(y)
+30
+z=40
+print(z)
+40
+3=50
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a3=50
+print(a3)
+50
+4a=90
+SyntaxError: invalid decimal literal
+print(4a)
+SyntaxError: invalid decimal literal
+a0123456789=100
+print(a0123456789)
+100
+name="sudharshan"
+print(name)
+sudharshan
+print("name")
+name
+city="vij"
+print(city)
+vij
+country"india"
+SyntaxError: invalid syntax
+@a=40
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+_=20
+print(_)
+20
+_b=100
+print(_b)
+100
+mailid="madasu@gamil.com"
+print(mailid)
+madasu@gamil.com
+if=30
+SyntaxError: invalid syntax
+elif=30
+SyntaxError: invalid syntax
+print=10
+print(print)
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    print(print)
+TypeError: 'int' object is not callable
+>>> a=4,b=4
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a=4;b=9
+>>> print(a+b)
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    print(a+b)
+TypeError: 'int' object is not callable
+>>> a,b=2,3
+>>> print(a+b)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(a+b)
+TypeError: 'int' object is not callable
+>>> a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
